a6ddd7cd4dd2473a21ead7a44d402f342716ccd4
silverware13 <thomasza@oregonstate.edu>
2018-02-14 15:16:20 -0800

    Comments and blank line handling.

--- smallsh.c
+++ smallsh.c
@@ -36,8 +36,6 @@ void userInput() {
 	size_t bufSize = 2048;
 	size_t characters;
 	buffer = (char *)malloc(bufSize * sizeof(char));
-	char *inStr;
-	inStr = (char *)malloc(bufSize * sizeof(char)); 
 	
 	while(1) {
 	
@@ -45,10 +43,32 @@ void userInput() {
 		printf(":");		
 		
 		// Get user input as a string.
-		*inStr = getline(&buffer, &bufSize, stdin);
-		
-		// Print user input as a string.
-		printf("%s", buffer);
+		getline(&buffer, &bufSize, stdin);
+	
+		// Check if input is just whitespace.
+		int emptyEnter = 1;
+		for(int i = 0; i < bufSize; i++) {
+	
+			// Check for non-white space chars.
+			if( buffer[i] != ' ' && buffer[i] != '\t' && buffer[i] != '\0' && buffer[i] != '\n' ){
+				emptyEnter = 0;
+				break;				
+			}
+
+			// If we have a null char we end early.
+			if(buffer[i] != '\0'){
+				break;
+			}
+
+		}			
+	
+		// Ignore the input if it is a comment or whitespace.
+		if(buffer[0] != '#' && emptyEnter == 0){	
+			
+			// Print user input as a string.
+			printf("%s", buffer);
+
+		}
 
 		// Flush stdout. 
 	        fflush(stdout);	
