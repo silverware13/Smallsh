ab7abbdf9b56c195099bd55fc48589b5e27e19d3
silverware13 <thomasza@oregonstate.edu>
2018-02-17 19:49:46 -0800

    Fixed issue with args.

--- smallsh.c
+++ smallsh.c
@@ -10,6 +10,8 @@
 //
 
 #define _GNU_SOURCE
+#define MAX_CHARS 2048
+#define MAX_ARGS 512
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,17 +35,15 @@ void userInput() {
 
 	// Buffer info.
 	char *buffer;
-	size_t bufSize = 2048;
+	size_t bufSize = MAX_CHARS;
 	size_t characters;
 	buffer = (char *)malloc(bufSize * sizeof(char));
 	
 	// Stores user command.
 	char command[50];
-	memset(command, '\0', sizeof(command));
 	
-	// Stores up to 512 arguments.
-	char arg[512][50];
-	memset(arg, '\0', sizeof(arg[0][0]) * 512 * 50);
+	// Stores arguments.
+	char arg[MAX_ARGS][50];
 	
 	while(1) {
 	
@@ -74,10 +74,13 @@ void userInput() {
 		if(buffer[0] != '#' && emptyEnter == 0){	
 			
 			// Save command and arguments.
+			memset(command, '\0', sizeof(command)); // We clear command before reading in.
+			memset(arg, '\0', sizeof(arg[0][0]) * MAX_ARGS * 50); // We clear args before reading in.
+			int bytesConsumed = 0, bytesNow = 0; // Keep track of the number of bytes we have travled in the string.
  			sscanf(buffer, "%s", command);
-			for(int i = 0; sscanf(buffer, "%s",  arg[i]) == 1; i++ ){};	
-
-			printf("%s %s %s %s %s", command, arg[1], arg[2], arg[3], arg[4]);
+			for(int i = 0; i < MAX_ARGS && sscanf(buffer + bytesConsumed, "%s%n", arg[i], & bytesNow) == 1; i++) {
+				bytesConsumed += bytesNow;
+			}	
 			
 			// React to command.
 			if(strcmp(command,"exit") == 0){
@@ -86,8 +89,7 @@ void userInput() {
 			} else if(strcmp(command,"exit") == 0){
 				exit(0);
 			} else if(strcmp(command,"cd") == 0){
-				cd test;
-				//exit(0);
+				exit(0);
 			} else if(strcmp(command,"status") == 0){
 				exit(0);
 			} else {
