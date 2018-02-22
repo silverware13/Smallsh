881b6544be32304ce9bd8e9ea7d3562360629842
silverware13 <thomasza@oregonstate.edu>
2018-02-21 23:38:37 -0800

    Basic starting redirect.

--- smallsh.c
+++ smallsh.c
@@ -242,42 +242,47 @@ void forkExe(char **args, pid_t *lastPID, int *lastExit) {
 		// This process is the child, execute command.
 		case 0: {
 
-			// Perform any needed input / output redirection.
-			// use dup2()? Don't pass dest/source into the exec.
-			// Setup background processes if they are called.
 			
-			// FIX SEGFAULT FOR THIS LINE.
-			//for(int i = 0; i < MAX_ARGS; i++) {
-			// We look through arguments for an output redirect.
+			// We look through arguments for an input/output redirect.
 			for(int i = 0; i < MAX_ARGS; i++) {
-			
-				if( args[i] != '\0') {	
+			        
+				// If the next argument is NULL break.
+				if( args[i] == '\0') {
+					break;
+				}	
+
 				// Check for output redirect.
 				if( strcmp(args[i], ">") == 0 ){
-					printf("FOUND > \n");
-					break;				
-				} else {
-					printf("args[0] : %s\n", args[i]);
+					// Open output file.
+					int targetFD = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+					if (targetFD == -1) { perror("open()"); exit(1); }
+					// Redirect output.
+					int result = dup2(targetFD, 1);
+					if (result == -1) { perror("dup2"); exit(2); }
+					// Set the redirect argument to NULL so we can pass args to exe.
+					args[i] = '\0';
+					break;
 				}
-				}
-
-				/*	
+				
 				// Check for input redirect.
 				if( strcmp(args[i], "<") == 0 ){
-					printf("FOUND < \n");
-					break;				
+					// Open input file.
+					int targetFD = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+					if (targetFD == -1) { perror("open()"); exit(1); }
+					// Redirect input.
+					int result = dup2(targetFD, 0);
+					if (result == -1) { perror("dup2"); exit(2); }
+					// Set the redirect argument to NULL so we can pass args to exe.
+					args[i] = '\0';
+					break;
 				}
-				*/
-				//printf("%i\n", i);
-				// Check for background.				
-
-				// If we have a null char we end early.
-				//if(buffer[i] == '\0'){
-				//	break;
-				//}
 
 			}			
 
+			//!! Make sure we close and reopen files so they aren't the same in parent.
+			
+			//!! Setup background processes if they are called.
+			
 			execvp(args[0], args); // Execute command.
 			perror("Command could not be executed.\n"); // Only get error if process never executed.
 			exit(1);
