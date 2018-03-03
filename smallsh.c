5770ab0aa3f9370f9c15df3528f0587e792ca097
silverware13 <thomasza@oregonstate.edu>
2018-03-02 17:26:29 -0800

    error handling.

--- smallsh.c
+++ smallsh.c
@@ -385,7 +385,7 @@ void forkExe(char **args, pid_t *lastPID, int *lastExit) {
 				if( strcmp(args[i], ">") == 0 ){
 					// Open output file.
 					int targetFD = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-					if (targetFD == -1) { perror("cannot open for output: "); exit(1); }
+					if (targetFD == -1) { perror("cannot open for output"); exit(1); }
 					// Redirect output.
 					int result = dup2(targetFD, 1);
 					if (result == -1) { perror("dup2"); exit(2); }
@@ -397,7 +397,7 @@ void forkExe(char **args, pid_t *lastPID, int *lastExit) {
 				if( strcmp(args[i], "<") == 0 ){
 					// Open input file.
 					int sourceFD = open(args[i+1], O_RDONLY);
-					if (sourceFD == -1) { perror("cannot open for input: "); exit(1); }
+					if (sourceFD == -1) { perror("cannot open for input"); exit(1); }
 					// Redirect input.
 					int result = dup2(sourceFD, 0);
 					if (result == -1) { perror("dup2"); exit(2); }
